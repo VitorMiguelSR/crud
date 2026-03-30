366e103f6d92bcf5bd66285e2c430f1379a269e8
VitorMiguelSR <vitor.redondo@escola.pr.gov.br>
2026-03-30 08:55:30 -0300

    aula 30/03/2026

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
+
+use const Dom\INDEX_SIZE_ERR;
 include("conexao.php");
 $sql = "SELECT * FROM produtos ORDER BY id Desc";
 $resultado = mysqli_query($conexao, $sql);
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width='device-width', initial-scale=1.0">
+    <title>INDEX</title>
+</head>
+<body>
+    <h1>Cadastro</h1>
+    <form action="salvar.php" method="post">
+        <label>Nome:</label>
+        <input type="text" name="Nome" required>
+        <label>Preço:</label>
+        <input type="number" name="Preço" required>
+        <label>Categoria:</label>
+        <input type="text" name="Categoria" required>
+        <label>Quantidades:</label>
+        <input type="number" name="Quantidades" required> 
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
 ?>
